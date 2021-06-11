4c291a555d30c124a7e1fdb47e4daf71eec5c846
efrain9213 <83841041+efrain9213@users.noreply.github.com>
2021-06-11 18:34:35 -0500

    Create efrainhola.php
    R.A. 2.1. Desarrollo de código de programación en un lenguaje script

---
+++ efrainhola.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Archivo PHP que muestra una imagen</title>
+</head>
+<body background="https://th.bing.com/th/id/Rf71935b950c80ac3e8fd090af751e6c1?rik=1kYe70pDqLeKEQ&pid=ImgRaw">
+<h1> <center> <font color='purple'><font face='Algerian'> Primera práctica en php:</center> <h1>
+<br />
+<?PHP
+echo "<center><font color='purple'><font face='Algerian' size='6'>Hola Mundo Efrain San Roman Corro</font></center>";
+
+?>
+</body>
+</html>
